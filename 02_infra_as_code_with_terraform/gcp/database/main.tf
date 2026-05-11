88a0123f92571158a77bff8133621146c8b03f5e
zagani212 <abdelhakassahi@gmail.com>
2026-05-11 21:55:44 +0200

    set edition for cloud sql

--- 02_infra_as_code_with_terraform/gcp/database/main.tf
+++ 02_infra_as_code_with_terraform/gcp/database/main.tf
@@ -1,11 +1,12 @@
 resource "google_sql_database_instance" "instance" {
   name                = "my-database-instance"
   region              = var.region
-  database_version    = "POSTGRES_18"
   deletion_protection = false
+  database_version    = "POSTGRES_18"
 
   settings {
     tier = "db-f1-micro"
+    edition = "ENTERPRISE"
     # ip_configuration {
     #   ipv4_enabled    = false # Disables Public IP
     #   private_network = var.vpc_network_id
--- 02_infra_as_code_with_terraform/gcp/providers.tf
+++ 02_infra_as_code_with_terraform/gcp/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0.0" # Use a modern version for 2026
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
